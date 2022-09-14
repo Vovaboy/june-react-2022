e29971e4183d1a17f09dd4b6b1f44cf560c83503
Vovaboy <vovaboy9510@gmail.com>
2022-09-14 21:28:22 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Users from "./components/users/Users";
+
+function App() {
+
+
+
+  return (<div>
+    <h2>app component header</h2>
+    <Users/>
+
+  </div>);
+}
+
+export default App;
