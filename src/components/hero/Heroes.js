52d82482f59ab93fd1b431e776f4c7461b5beb44
Vovaboy <vovaboy9510@gmail.com>
2022-09-15 20:01:06 +0200

    hw-1

--- src/components/hero/Heroes.js
+++ src/components/hero/Heroes.js
@@ -1,20 +1,22 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import Heroe from "./Hero"
 
 function Heroes() {
 
     const [characters, setCharacters] = useState([]);
+    useEffect(() => {
+        fetch(`https://rickandmortyapi.com/api/character`)
+            .then(value => value.json())
+            .then(value => {
+                setCharacters(value.results);
+            });
+    })
 
-    fetch(`https://rickandmortyapi.com/api/character`)
-        .then(value => value.json())
-        .then(value => {
-            setCharacters(value.results);
-        });
 
     return (
         <div>
             {
-                characters.map(character => <Heroe key={character.id} character={character} />)
+                characters.map(character => <Heroe key={character.id} character={character}/>)
             }
         </div>
     );
